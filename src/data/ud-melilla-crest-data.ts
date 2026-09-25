4d21512fa37f56acba24ab533b29a49635a1586e
masdeportemelillense-collab <masdeportemelillense@gmail.com>
2026-09-25 10:02:53 +0200

    Foto exacta del escudo UD Melilla (sin redibujar).

--- src/data/ud-melilla-crest-data.ts
+++ src/data/ud-melilla-crest-data.ts
@@ -1,1 +1,5 @@
-placeholder
+import { P0 } from "./crest-ud-p0";
+import { P1 } from "./crest-ud-p1";
+import { P2 } from "./crest-ud-p2";
+import { P3 } from "./crest-ud-p3";
+export const UD_MELILLA_CREST = "data:image/jpeg;base64," + P0 + P1 + P2 + P3;
